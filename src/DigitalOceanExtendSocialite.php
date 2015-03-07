73d55149e0047f5d430aa9f69d4f5e141859fce0
DraperStudio <hello@draperstud.io>
2015-03-07 11:58:51 +0100

    Update comments

--- src/DigitalOceanExtendSocialite.php
+++ src/DigitalOceanExtendSocialite.php
@@ -6,7 +6,9 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 class DigitalOceanExtendSocialite
 {
     /**
-     * Execute the provider.
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
      */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
